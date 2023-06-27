aecbb43eea8e2df1fab964c182dcefc3368a0a0f
pedrot111 <pedrochincolla11@gmail.com>
2023-06-27 11:57:33 -0300

    add routes

--- micafe-server/src/app.ts
+++ micafe-server/src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import userRoutes from './routes/userRoutes';
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+
+app.use('/authenticate', userRoutes);
 export default app;
--- micafe-server/src/config.ts
+++ micafe-server/src/config.ts
@@ -5,6 +5,10 @@ dotenv.config();
 const config = {
     PORT: process.env.PORT,
     DATABASE: process.env.DATABASE || '',
+    DATABASE_NAME: process.env.DATABASE_NAME,
+    JWT: process.env.JWT_SECRET || '',
+    JWT_EXPIRE: process.env.JWT_EXPIRES_IN,
 };
+
 
 export default config;
