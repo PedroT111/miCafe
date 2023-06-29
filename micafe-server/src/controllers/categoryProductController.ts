7d3dbf9039dc3ed27a18ffef7d82f0768b8988ab
pedrot111 <pedrochincolla11@gmail.com>
2023-06-29 16:22:00 -0300

    implement crud operations for categories

---
+++ micafe-server/src/controllers/categoryProductController.ts
@@ -0,0 +1,70 @@
+import { Request, Response, NextFunction } from 'express';
+import catchAsync from '../utils/catchAsync';
+import AppError from '../utils/appError';
+import {
+  findOneByName,
+  createOne,
+  findAll,
+  detail,
+  update
+} from '../services/categoryProduct';
+
+export const createCategory = catchAsync(
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const { name } = req.body;
+    const category = await findOneByName(name);
+    if (category) {
+      return next(
+        new AppError('There is already a category with that name.', 409)
+      );
+    }
+    const newCategory = req.body;
+    await createOne(newCategory);
+    res.status(201).json({
+      ok: true,
+      newCategory
+    });
+  }
+);
+
+export const getCategories = catchAsync(
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const categories = await findAll();
+    if (!categories) {
+      return next(new AppError('Something was wrong', 400));
+    }
+    res.status(200).json({
+      ok: true,
+      categories
+    });
+  }
+);
+
+export const getOne = catchAsync(
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const { id } = req.params;
+    const category = await detail(id);
+    if (!category) {
+      return next(new AppError('Something was wrong!', 400));
+    }
+    res.status(200).json({
+      ok: true,
+      category
+    });
+  }
+);
+
+export const updateCategory = catchAsync( async(req:Request, res:Response, next: NextFunction): Promise<void> => {
+    const {id} = req.params;
+    const categoryToEdit = {...req.body};
+    const query = await update(id, categoryToEdit);
+    if(!query){
+        return next(new AppError('Something was wrong', 400));
+    }
+    res.status(200).json({
+        ok:true,
+        query
+    })
+
+
+})
---
+++ micafe-server/src/services/categoryProduct.ts
@@ -0,0 +1,39 @@
+import {
+  CategoryProduct,
+  ICategoryProduct
+} from '../models/categoryProductModel';
+
+export const createOne = async (
+  categoryData: ICategoryProduct
+): Promise<ICategoryProduct> => {
+  const category = new CategoryProduct(categoryData);
+  return await category.save();
+};
+
+export const findOneByName = async (
+  name: string
+): Promise<ICategoryProduct | null> => {
+  return await CategoryProduct.findOne({ name });
+};
+
+export const findAll = async (): Promise<ICategoryProduct[]> => {
+  return await CategoryProduct.find({ isDeleted: false });
+};
+
+export const detail = async (id: any): Promise<ICategoryProduct | null> => {
+  return await CategoryProduct.findById(id);
+};
+
+export const update = async (
+  id: any,
+  data: ICategoryProduct
+): Promise<ICategoryProduct | null> => {
+  return await CategoryProduct.findByIdAndUpdate(
+    id,
+    {
+      ...data,
+      updateAt: new Date()
+    },
+    { new: true }
+  );
+};
