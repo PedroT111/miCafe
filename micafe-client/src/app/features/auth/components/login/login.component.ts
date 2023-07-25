8297ad934a44d968708640fd0b78d63099bc327e
pedrot111 <pedrochincolla11@gmail.com>
2023-07-25 12:20:12 -0300

    add onSubmit function and use toastr to show errors

--- micafe-client/src/app/features/auth/components/login/login.component.ts
+++ micafe-client/src/app/features/auth/components/login/login.component.ts
@@ -1,24 +1,55 @@
-import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormBuilder, Validators} from '@angular/forms';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css', '../../styles/shared-styles.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnDestroy, OnInit {
+  sub: Subscription = new Subscription();
   loginForm: FormGroup;
-  constructor(private formBuilder: FormBuilder) { }
-
-  ngOnInit(): void {
+  constructor(
+    private toastr: ToastrService,
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(8)]],
-    })
-  }
-  hasError(controlName: string, errorName: string): boolean {
-    const control = this.loginForm.get(controlName);
-    return control?.errors && control?.touched && control?.errors[errorName];
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
   }
 
+  ngOnInit(): void {}
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  hasError(controlName: string, error: string): boolean {
+    return !!this.loginForm.get(controlName)?.hasError(error);
+  }
+
+  onSubmit() {
+    this.loginForm.markAllAsTouched();
+    if (this.loginForm.valid) {
+      this.sub.add(
+        this.authService.login(this.loginForm.value).subscribe({
+          next: (res) => {
+            this.authService.saveToken(res.token);
+            this.authService.saveUserData(res.user);
+            //redirigir al inicio
+            this.router.navigate(['/']);
+          },
+          error: (err) => {
+            console.log(err);
+            this.toastr.error(err.error.message, 'Error');
+          }
+        })
+      );
+    }
+  }
 }
