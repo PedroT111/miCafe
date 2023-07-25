27404a36935fcf8fc7310985cab98661b6e15d81
pedrot111 <pedrochincolla11@gmail.com>
2023-07-25 12:04:13 -0300

    create not found page and add routing

--- micafe-client/src/app/app-routing.module.ts
+++ micafe-client/src/app/app-routing.module.ts
@@ -1,17 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './features/home/index/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
   {
-    path: '', component: HomeComponent
+    path: '',
+    component: HomeComponent
   },
-  {path:'auth', loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule)}
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./features/auth/auth.module').then((m) => m.AuthModule)
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
 ];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
