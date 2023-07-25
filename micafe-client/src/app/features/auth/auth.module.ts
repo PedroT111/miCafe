6f1174a3c0f2773a31ef4d46af3a4435d7953935
pedrot111 <pedrochincolla11@gmail.com>
2023-07-25 12:15:49 -0300

    add method to check if the user is logged in

--- micafe-client/src/app/features/auth/auth.module.ts
+++ micafe-client/src/app/features/auth/auth.module.ts
@@ -5,13 +5,15 @@ import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { AuthComponent } from './auth/auth.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { PasswordResetComponent } from './components/password-reset/password-reset.component';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
     SignupComponent,
-    AuthComponent
+    AuthComponent,
+    PasswordResetComponent,
   ],
   imports: [
     CommonModule,
--- micafe-client/src/app/features/auth/auth/auth.component.ts
+++ micafe-client/src/app/features/auth/auth/auth.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from 'src/app/core/services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private toastr: ToastrService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    if (this.authService.isLoggedIn()) {
+      this.toastr.info('Ya iniciaste sesión!');
+      this.router.navigate(['/']);
+    }
   }
-
 }
