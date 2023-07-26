72e217a4cf851e9eac09d5e0e27d6d0b0b2ef6c0
pedrot111 <pedrochincolla11@gmail.com>
2023-07-26 15:39:36 -0300

    create form-validation and form-error directive

---
+++ micafe-client/src/app/shared/directives/form-error.directive.ts
@@ -0,0 +1,48 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Directive, ElementRef,Input,} from '@angular/core';
+import { AbstractControl, ValidationErrors } from '@angular/forms';
+
+@Directive({
+  selector: '[appFormError]'
+})
+export class FormErrorDirective /*implements DoCheck*/{
+  @Input() control!: AbstractControl | null;
+
+  constructor(private element: ElementRef) { }
+
+  private controlErrors: { [key: string]: string } = {
+    required: 'Campo obligatorio',
+    email: 'Ingrese un email válido',
+    minlength: 'Mínimo 8 caracteres',
+    emailExisting: 'El email ingresado ya está registrado'
+  }
+
+
+  private updateView(errors: ValidationErrors | null | undefined) {
+    console.log(errors)
+    if (errors) {
+      Object.keys(errors).forEach(errorKey => {
+        const errorMessage = this.controlErrors[errorKey];
+        if(errorMessage){
+          this.element.nativeElement.textContent = errorMessage;
+        } else {
+          this.element.nativeElement.textContent = '';
+          this.element.nativeElement.style.display = 'none';
+        }
+      })
+    }
+  }
+
+  ngOnInit() {
+    if (this.control) {
+      this.updateView(this.control?.errors);
+      this.control.statusChanges.subscribe(() => {
+        this.updateView(this.control?.errors);
+      });
+    }
+  }
+  
+  
+}
+
+  
