060b7e883fd05ef2b95fa3ed046065ea0dd72b51
pedrot111 <pedrochincolla11@gmail.com>
2023-07-31 18:26:22 -0300

    Modify navbar to add items from constants

--- micafe-client/src/app/shared/components/navbar/navbar.component.ts
+++ micafe-client/src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { User } from '../../models/user';
+import { navigationNavbar, loggedNavigationNavbar } from '../../constants/navigation';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  navigationNavbar = navigationNavbar;
+  loggedNavigationNavbar = loggedNavigationNavbar;
+  isLoggedIn: boolean = false;
+  user: User | null;
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.isLoggedIn = this.authService.isLoggedIn();
+    this.user = this.authService.getUserData();
+  }
+
+  logOut(){
+    this.authService.logOut();
+    setTimeout(() => {
+      this.isLoggedIn = this.authService.isLoggedIn();
+    }, 2000);
   }
 
 }
