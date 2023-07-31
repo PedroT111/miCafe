288be126eb69d02c8fc32ef9a802f96f989df1e0
pedrot111 <pedrochincolla11@gmail.com>
2023-07-31 18:23:40 -0300

    add navigation constants

---
+++ micafe-client/src/app/shared/constants/index.ts
@@ -0,0 +1,17 @@
+export const PAGES = {
+  INDEX: '',
+  AUTH: 'auth',
+  AUTH_LOGIN: 'login',
+  AUTH_SIGNUP:'signup',
+  AUTH_FORGET_PASSWORD: 'forget-password',
+  AUTH_RESET_PASSWORD: 'password-reset',
+  TIENDA: 'tienda',
+  US: 'nosotros',
+  PROFILE: 'perfil',
+  HISTORIAL: 'historial',
+  ADMIN: 'admin',
+  ADMIN_DASHBOARD: 'dashboard',
+  ADMIN_PRODUCTS: 'products',
+  ADMIN_CUSTOMERS: 'customers',
+  ADMIN_REPORTS: 'reports'
+};
---
+++ micafe-client/src/app/shared/constants/navigation.ts
@@ -0,0 +1,52 @@
+import { PAGES } from './index';
+export const navigationNavbar = [
+  {
+    name: 'Home',
+    href: PAGES.INDEX,
+    icon: 'bi bi-house-fill'
+  },
+  {
+    name: 'Tienda',
+    href: PAGES.TIENDA,
+    icon: 'bi bi-house-fill'
+  },
+  {
+    name: 'Nosotros',
+    href: PAGES.US,
+    icon: 'bi bi-house-fill'
+  }
+];
+
+export const loggedNavigationNavbar = [
+  {
+    name: 'Mi Perfil',
+    href: PAGES.PROFILE
+  },
+  {
+    name: 'Historial Pedidos',
+    href: PAGES.HISTORIAL
+  }
+];
+
+export const adminNavigationNavbar = [
+  {
+    name: 'Dashboard',
+    href: PAGES.ADMIN_DASHBOARD,
+    icon: 'bi bi-house-fill'
+  },
+  {
+    name: 'Products',
+    href: PAGES.ADMIN_PRODUCTS,
+    icon: 'bi bi-cup-hot-fill'
+  },
+  {
+    name: 'Customers',
+    href: PAGES.ADMIN_PRODUCTS,
+    icon: 'bi bi-house-fill'
+  },
+  {
+    name: 'Reports',
+    href: PAGES.ADMIN_REPORTS,
+    icon: 'bi bi-bar-chart-fill'
+  },
+]
