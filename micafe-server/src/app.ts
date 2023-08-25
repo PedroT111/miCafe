5e9036a4dd952dd5745b28040b4cd52a16013743
pedrot111 <pedrochincolla11@gmail.com>
2023-08-25 13:43:38 -0300

    activate tasks

--- micafe-server/src/app.ts
+++ micafe-server/src/app.ts
@@ -1,9 +1,13 @@
-import express from 'express';
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+import express, { type ErrorRequestHandler } from 'express';
 import cors from 'cors';
 import userRoutes from './routes/userRoutes';
 import categoryProductRoutes from './routes/categoryProductRoutes';
 import productRoutes from './routes/productRoutes';
 import comboRoutes from './routes/comboRoutes';
+import offerRoutes from './routes/offerRoutes';
+import AppError from './utils/appError';
+import offerScheduler from './tasks/offerScheduler';
 
 const app = express();
 app.use(cors());
@@ -14,4 +18,24 @@ app.use('/authenticate', userRoutes);
 app.use('/category-product', categoryProductRoutes);
 app.use('/product', productRoutes);
 app.use('/combo', comboRoutes);
+app.use('/offer', offerRoutes);
+
+offerScheduler();
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  let statusCode = 500;
+  let message = 'Something went wrong';
+
+  if (err instanceof AppError) {
+    statusCode = err.statusCode || 500;
+    message = err.message;
+  }
+
+  res.status(statusCode).json({
+    error: message
+  });
+};
+
+app.use(errorHandler);
+
 export default app;
