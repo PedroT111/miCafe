5c1ef9d0c5091ec99165b50d2117c75414205de9
pedrot111 <pedrochincolla11@gmail.com>
2023-08-25 16:14:07 -0300

    create admin routes

--- micafe-client/src/app/features/admin/admin-routing.module.ts
+++ micafe-client/src/app/features/admin/admin-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './index/index.component';
+import { ProductsComponent } from './product/products/products.component';
+import { ProductEditComponent } from './product/product-edit/product-edit.component';
+import { ProductCreateComponent } from './product/product-create/product-create.component';
+import { OffersListComponent } from './offers/offers-list/offers-list.component';
+import { NewOfferComponent } from './offers/new-offer/new-offer.component';
+import { EditOfferComponent } from './offers/edit-offer/edit-offer.component';
 
 const routes: Routes = [
   {
-    path: '', component: IndexComponent
+    path: '',
+    component: IndexComponent,
+    children: [
+      { path: 'products', component: ProductsComponent },
+      { path: 'products/edit/:id', component: ProductEditComponent },
+      { path: 'products/new', component: ProductCreateComponent },
+      {path: 'offers', component: OffersListComponent},
+      {path: 'offers/new', component: NewOfferComponent},
+      {path: 'offers/edit/:id', component: EditOfferComponent}
+    ]
   }
 ];
 
@@ -12,4 +27,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AdminRoutingModule { }
+export class AdminRoutingModule {}
--- micafe-client/src/app/features/admin/admin.module.ts
+++ micafe-client/src/app/features/admin/admin.module.ts
@@ -5,17 +5,55 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { SideNavbarComponent } from './components/side-navbar/side-navbar.component';
 import { IndexComponent } from './index/index.component';
+import { ProductsComponent } from './product/products/products.component';
+import { TableComponent } from './components/table/table.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { OrderByComponent } from './components/order-by/order-by.component';
+import { ProductFormComponent } from './components/forms/product-form/product-form.component';
+import { ProductEditComponent } from './product/product-edit/product-edit.component'; 
+import { ProductCreateComponent } from './product/product-create/product-create.component';
+import { NgbPaginationModule, NgbDatepickerModule, NgbTimepickerModule } from '@ng-bootstrap/ng-bootstrap';
+import { CategoryProductsComponent } from './product/category-products/category-products.component';
+import { OffersListComponent } from './offers/offers-list/offers-list.component';
+import { OfferFormComponent } from './components/forms/offer-form/offer-form.component';
+import { NewOfferComponent } from './offers/new-offer/new-offer.component';
+import { DatepickerComponent } from './components/datepicker/datepicker.component';
+import { TimepickerComponent } from './components/timepicker/timepicker.component';
+import { EditOfferComponent } from './offers/edit-offer/edit-offer.component';
+import { PriceAdjustemFormComponent } from './components/forms/price-adjustem-form/price-adjustem-form.component';
+
 
 
 @NgModule({
   declarations: [
     IndexComponent,
-    SideNavbarComponent
+    SideNavbarComponent,
+    ProductsComponent,
+    TableComponent,
+    OrderByComponent,
+    ProductFormComponent,
+    ProductEditComponent,
+    ProductCreateComponent,
+    CategoryProductsComponent,
+    OffersListComponent,
+    OfferFormComponent,
+    NewOfferComponent,
+    DatepickerComponent,
+    TimepickerComponent,
+    EditOfferComponent,
+    PriceAdjustemFormComponent,
   ],
   imports: [
     CommonModule,
     AdminRoutingModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbPaginationModule,
+    NgbDatepickerModule,
+    NgbTimepickerModule
   ]
 })
 export class AdminModule { }
