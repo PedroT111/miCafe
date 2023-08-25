a4e562cf73845a063ac3b32249035566da8640e6
pedrot111 <pedrochincolla11@gmail.com>
2023-08-25 16:10:51 -0300

    import formsModule

--- micafe-client/src/app/shared/shared.module.ts
+++ micafe-client/src/app/shared/shared.module.ts
@@ -3,29 +3,40 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import {RouterModule} from '@angular/router';
-import {ReactiveFormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { FormValidationDirective } from './directives/form-validation.directive';
 import { FormErrorDirective } from './directives/form-error.directive';
-
-
+import { FilterComponent } from './components/filter/filter.component';
+import { SearchComponent } from './components/search/search.component';
+import { TruncateTextPipe } from './pipes/truncate-text.pipe';
+import { SpinnerComponent } from './components/spinner/spinner.component';
 
 @NgModule({
   declarations: [
     NavbarComponent,
     FooterComponent,
     FormValidationDirective,
-    FormErrorDirective
+    FormErrorDirective,
+    FilterComponent,
+    SearchComponent,
+    TruncateTextPipe,
+    SpinnerComponent,
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    FormsModule,
   ],
   exports: [
     NavbarComponent,
     FooterComponent,
     ReactiveFormsModule,
     FormValidationDirective,
-    FormErrorDirective
+    FormErrorDirective,
+    FilterComponent,
+    SearchComponent,
+    TruncateTextPipe,
+    
   ]
 })
 export class SharedModule { }
