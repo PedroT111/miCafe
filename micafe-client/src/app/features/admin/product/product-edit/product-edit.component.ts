e84db17f7ecd36485a739ead3ece50ca5b4961a7
pedrot111 <pedrochincolla11@gmail.com>
2023-08-25 13:34:09 -0300

    create edit product page

---
+++ micafe-client/src/app/features/admin/product/product-edit/product-edit.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ProductsService } from '../../services/products.service';
+import { Product } from 'src/app/shared/models/product';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+import { SwalService } from 'src/app/shared/utils/swal.service';
+import { PAGES } from 'src/app/shared/constants';
+
+@Component({
+  selector: 'app-product-edit',
+  templateUrl: './product-edit.component.html',
+  styleUrls: ['./product-edit.component.css']
+})
+export class ProductEditComponent implements OnInit, OnDestroy {
+  sub: Subscription = new Subscription();
+
+  constructor(
+    private productService: ProductsService,
+    private toastr: ToastrService,
+    private router: Router,
+    private swalService: SwalService
+  ) {}
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  ngOnInit(): void {}
+
+  updateProduct(p: Product) {
+    this.swalService
+      .showConfirmation(
+        '"Estás a punto de guardar los cambios en este producto. ¿Deseas continuar?"'
+      )
+      .then((res) => {
+        if (res.isConfirmed) {
+          this.sub.add(
+            this.productService.editProduct(p).subscribe({
+              next: (res) => {
+                if (res.ok == true) {
+                  this.router.navigate([`/admin/${PAGES.ADMIN_PRODUCTS}`]);
+                  this.toastr.success('Producto actualizado correctamente!');
+                }
+              },
+              error: (err) => {
+                this.toastr.error(err.error.error);
+              }
+            })
+          );
+        }
+      });
+  }
+}
