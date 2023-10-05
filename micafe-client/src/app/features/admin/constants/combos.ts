4de69a10a0c536efc5519369f3061d553e31dbb8
pedrot111 <pedrochincolla11@gmail.com>
2023-10-05 10:48:46 -0300

    add combo table headers

---
+++ micafe-client/src/app/features/admin/constants/combos.ts
@@ -0,0 +1,29 @@
+export const COMBO = {
+    COMBO_TABLE_HEADERS: [
+      {
+        header: 'Nombre',
+        name: 'name'
+      },
+      {
+        header: 'Descripcion',
+        name: 'description'
+      },
+      {
+        header: 'Precio',
+        name: 'price'
+      },
+      {
+        header: 'Puntos',
+        name: 'points'
+      },
+      {
+        header: 'Imagen',
+        name: 'urlImage'
+      },
+      {
+        header: 'Estado',
+        name: 'isActive'
+      },
+    ],
+  };
+  
