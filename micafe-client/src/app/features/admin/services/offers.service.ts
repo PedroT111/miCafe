a7849d0e0e70bc4ad9278d38a95a91f198ebc416
pedrot111 <pedrochincolla11@gmail.com>
2023-10-05 11:57:40 -0300

    add create offer function

--- micafe-client/src/app/features/admin/services/offers.service.ts
+++ micafe-client/src/app/features/admin/services/offers.service.ts
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import {
   Offer,
   OfferApiResponse,
+  OfferCategory,
   OfferProduct,
   OffersList
 } from 'src/app/shared/models/offer';
@@ -18,20 +20,31 @@ export class OffersService {
     return this.http.post<OfferApiResponse>('/offer/new', offer);
   }
 
+  createOfferByCategory(offer: OfferCategory):Observable<OfferCategory>{
+    return this.http.post<OfferCategory>('/offer/category', offer);
+  }
+
+  getOffer(id: any):Observable<OfferApiResponse>{
+    return this.http.get<OfferApiResponse>(`/offer/${id}`);
+  }
+
   getOffers(): Observable<OfferProduct[]> {
     return this.http.get<OffersList>('/offer').pipe(
       map((res: OffersList) => {
-        console.log('res', res)
         const offers = res.offers;
         if(offers === undefined || offers.length === 0){
           return [];
         }
         return offers.map((offer) =>{
+          const isPercentage = offer.isPercentage;
+          const salePrice = isPercentage ? (offer.productId.price * (100 - offer.value)/100) : offer.value;
+          const percentage = (1 - (salePrice / offer.productId.price))*100;
           return {
             _id: offer._id,
             productName: offer.productId.name,
             price: offer.productId.price,
-            salePrice: offer.productId.salePrice,
+            salePrice: salePrice,
+            percentage: `${percentage.toFixed()}%`,
             startSale: new Date(offer.startSale).toLocaleString(),
             endSale: new Date(offer.endSale).toLocaleString(),
             status: offer.status
@@ -41,7 +54,11 @@ export class OffersService {
     );
   }
 
+  updateOffer(offer: Offer):Observable<OfferApiResponse>{
+    return this.http.put<OfferApiResponse>(`/offer/${offer._id}`, offer);
+  }
+
   deleteOffer(o: OfferProduct):Observable<OfferProduct>{
-    return this.http.delete<OfferProduct>(`/offer/product/${o._id}`);
+    return this.http.delete<OfferProduct>(`/offer/${o._id}`);
   }
 }
