bcc68cd153a24cc7f168021c19ee5600cf26dce4
pedrot111 <pedrochincolla11@gmail.com>
2023-10-05 11:07:16 -0300

    add getTopClients and inactiveClients functions

--- micafe-server/src/services/user.ts
+++ micafe-server/src/services/user.ts
@@ -1,7 +1,12 @@
+import { endOfMonth, endOfWeek, startOfMonth, startOfWeek } from 'date-fns';
+import { type IUserWithOrderCountDTO } from '../Dto/userDto';
 import { User, type IUser } from '../models/userModel';
 
+export const getAllCustomers = async (): Promise<IUser[]> => {
+  return await User.find({ isDeleted: false, role: 'user' });
+};
 export const getOneByEmail = async (email: string): Promise<IUser | null> => {
-  return await User.findOne({ email });
+  return await User.findOne({ email, isDeleted: false });
 };
 
 export const getOneByToken = async (
@@ -14,6 +19,97 @@ export const getOneById = async (id: string): Promise<IUser | null> => {
   return await User.findById(id).select('-password -validationToken').lean();
 };
 
+export const getUsersWithOrderCount = async (
+  role: string
+): Promise<IUserWithOrderCountDTO[]> => {
+  return await User.aggregate([
+    {
+      $lookup: {
+        from: 'Orders',
+        localField: '_id',
+        foreignField: 'customer',
+        as: 'orders'
+      }
+    },
+    {
+      $match: {
+        isDeleted: false,
+        role
+      }
+    },
+    {
+      $project: {
+        _id: '$_id',
+        name: '$name',
+        lastName: '$lastName',
+        email: '$email',
+        orderCount: { $size: '$orders' },
+        registrationDate: '$registrationDate',
+        lastOrderDate: '$lastOrderDate',
+        isValidated: '$isValidated'
+      }
+    }
+  ]).exec();
+};
+
+export const getTopClients = async (
+  limit: number,
+  filter: 'week' | 'month'
+): Promise<IUser[]> => {
+  const startDate =
+    filter === 'week' ? startOfWeek(new Date()) : startOfMonth(new Date());
+
+  const endDate =
+    filter === 'week' ? endOfWeek(new Date()) : endOfMonth(new Date());
+  return await User.aggregate([
+    {
+      $lookup: {
+        from: 'orders',
+        localField: '_id',
+        foreignField: 'customer',
+        as: 'orders'
+      }
+    },
+    {
+      $addFields: {
+        totalPurchases: {
+          $sum: '$orders.totalAmount'
+        }
+      }
+    },
+    {
+      $match: {
+        'orders.date': {
+          $gte: startDate,
+          $lt: endDate
+        }
+      }
+    },
+    {
+      $sort: { totalPurchases: -1 }
+    },
+    {
+      $limit: limit
+    }
+  ]);
+};
+
+export const getInactiveUsers = async (
+  daysInactive: number
+): Promise<IUser[]> => {
+  const currentDate = new Date();
+  const inactiveDateThreshold = new Date(
+    currentDate.setDate(currentDate.getDate() - daysInactive)
+  );
+
+  const inactiveUsers = await User.find({
+    isDeleted: false,
+    role: 'user',
+    lastOrderDate: { $lt: inactiveDateThreshold }
+  });
+
+  return inactiveUsers;
+};
 export const createOne = async (userData: IUser): Promise<IUser> => {
   const user = new User(userData);
   return await user.save();
