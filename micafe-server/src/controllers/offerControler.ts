9d86f52acfb3074dc709294b683117602e53742b
pedrot111 <pedrochincolla11@gmail.com>
2023-10-05 11:26:19 -0300

    add update offer controller

--- micafe-server/src/controllers/offerControler.ts
+++ micafe-server/src/controllers/offerControler.ts
@@ -4,13 +4,21 @@ import {
   create,
   createByCategory,
   deleteOffer,
-  getOffersWithProducts
+  editOffer,
+  getOffersWithProducts,
+  getOne
 } from '../services/offerService';
 import AppError from '../utils/appError';
-import { getByCategory, getOne } from '../services/product';
+import { getByCategory } from '../services/product';
+import { validationResult } from 'express-validator';
 
 export const createOffer = catchAsync(
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+      return;
+    }
     const newOffer = await create({ ...req.body });
     if (newOffer == null) {
       next(new AppError('Something was wrong!', 400));
@@ -22,14 +30,32 @@ export const createOffer = catchAsync(
   }
 );
 
+export const getOffer = catchAsync(
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+      return;
+    }
+    const { id } = req.params;
+    const offer = await getOne(id);
+    if (offer === null) {
+      next(new AppError('Oferta no encontrada', 400));
+      return;
+    }
+    res.status(200).json({
+      ok: true,
+      offer
+    });
+  }
+);
+
 export const getOffers = catchAsync(
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const offers = await getOffersWithProducts();
     if (offers.length === 0) {
-      res.status(200).json({
-        ok: true,
-        msg: 'There are no offers.'
-      });
+      next(new AppError('No hay ofertas', 400));
+      return;
     }
     res.status(200).json({
       ok: true,
@@ -60,9 +86,43 @@ export const createOffersByCategory = catchAsync(
     });
   }
 );
-
-export const deleteOfferByProduct = catchAsync(
+export const updateOffer = catchAsync(
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+      return;
+    }
+    const { id } = req.params;
+    const offer = { ...req.body };
+    const existingOffer = await getOne(id);
+
+    if (existingOffer === null) {
+      next(new AppError('There is no offer with that id.', 404));
+    }
+
+    if (existingOffer?.status === 'active') {
+      next(new AppError('No se puede modificar una oferta activa', 400));
+    }
+
+    const query = await editOffer(id, offer);
+    if (query === null) {
+      next(new AppError('Something was wrong', 400));
+    }
+
+    res.status(200).json({
+      ok: true,
+      offer: query
+    });
+  }
+);
+export const removeOffer = catchAsync(
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+      return;
+    }
     const { id } = req.params;
     const deletedOffer = await deleteOffer(id);
 
