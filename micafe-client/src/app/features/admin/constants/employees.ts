7a743d33f11a5ba145c481e667cd12b8209fb1de
pedrot111 <pedrochincolla11@gmail.com>
2023-10-05 10:47:49 -0300

    add employee table headers

---
+++ micafe-client/src/app/features/admin/constants/employees.ts
@@ -0,0 +1,21 @@
+export const EMPLOYEES = {
+    EMPLOYEES_TABLE_HEADERS: [
+      {
+        header: 'Nombre',
+        name: 'name'
+      },
+      {
+        header: 'Apellido',
+        name: 'lastName'
+      },
+      {
+        header: 'Email',
+        name: 'email'
+      },
+      {
+        header: 'Fecha de Registro',
+        name: 'registrationDate'
+      }
+    ]
+  };
+  
