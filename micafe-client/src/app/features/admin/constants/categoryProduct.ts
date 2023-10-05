9f630fe6936b5ff4cadc694eea1e91c5ae0a8dc4
pedrot111 <pedrochincolla11@gmail.com>
2023-10-05 10:49:05 -0300

    add category table headers

---
+++ micafe-client/src/app/features/admin/constants/categoryProduct.ts
@@ -0,0 +1,16 @@
+export const CATEGORY = {
+    CATEGORY_TABLE_HEADERS: [
+        {
+            header: 'Nombre',
+            name: 'name'
+        },
+        {
+            header: 'Tipos de Productos',
+            name: 'totalProducts'
+        },
+    ],
+    SORTING:[
+        { sort: 'name', asc: true, name: 'Nombre asc' },
+        { sort: 'name', asc: false, name: 'Nombre desc' },
+      ]
+}
