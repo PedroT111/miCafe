ac6b3baf2c313695fd6e0271dcc34117d617a68f
pedrot111 <pedrochincolla11@gmail.com>
2023-10-05 11:38:24 -0300

    add offer category

--- micafe-client/src/app/shared/models/offer.ts
+++ micafe-client/src/app/shared/models/offer.ts
@@ -1,10 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Product } from "./product";
 
 export interface Offer{
     _id: string;
     productId: Product;
     isPercentage: boolean;
-    value: number
+    value: any;
     startSale: Date;
     endSale: Date;
     status: string;
@@ -15,9 +16,17 @@ export interface OfferProduct{
     productName: string;
     price: number;
     salePrice: number;
+    percentage: string;
     startSale: string;
     endSale: string;
     status: string;
+}
+
+export interface OfferCategory{
+    categoryId: string;
+    value: number;
+    startSale: Date;
+    endSale: Date;
 }
 
 export interface OfferApiResponse{
