8cdb56165e545c87150c7e4115b9176b9cc8fec2
pedrot111 <pedrochincolla11@gmail.com>
2023-10-05 11:08:49 -0300

    add function to edit offer

--- micafe-server/src/services/offerService.ts
+++ micafe-server/src/services/offerService.ts
@@ -1,14 +1,18 @@
 import { type IOffer, Offer } from '../models/offerModel';
 import { Product } from '../models/productModel';
 
+export const getOne = async (id: any): Promise<IOffer | null> => {
+  return await Offer.findById(id).populate('productId');
+};
+
 export const getOfferByProduct = async (
   productId: string
 ): Promise<IOffer | null> => {
   return await Offer.findOne({ productId });
 };
 
-export const create = async (o: IOffer): Promise<IOffer | null> => {
-  return await Offer.create(o);
+export const create = async (offer: IOffer): Promise<IOffer | null> => {
+  return await Offer.create(offer);
 };
 
 export const createByCategory = async (
@@ -38,8 +42,35 @@ export const getOffersWithProducts = async (): Promise<IOffer[]> => {
   });
 };
 
-export const deleteOffer = async (id: any): Promise<IOffer | null> => {
-  const offer = await Offer.findOne({ id, isDeleted: false });
+export const editOffer = async (
+  id: any,
+  offer: IOffer
+): Promise<IOffer | null> => {
+  const now = new Date();
+  const startSale =
+    offer.startSale instanceof Date
+      ? offer.startSale
+      : new Date(offer.startSale);
+  const endSale =
+    offer.endSale instanceof Date ? offer.endSale : new Date(offer.endSale);
+  if (endSale >= now && now >= startSale) {
+    offer.status = 'active';
+  } else if (startSale > now) {
+    offer.status = 'scheduled';
+  } else {
+    offer.status = 'expired';
+  }
+  return await Offer.findByIdAndUpdate(
+    id,
+    {
+      ...offer
+    },
+    { new: true }
+  );
+};
+
+export const deleteOffer = async (_id: any): Promise<IOffer | null> => {
+  const offer = await Offer.findOne({ _id, isDeleted: false });
   if (offer != null) {
     const product = await Product.findById(offer.productId);
 
@@ -54,7 +85,6 @@ export const deleteOffer = async (id: any): Promise<IOffer | null> => {
     offer.status = 'expired';
 
     await offer.save();
-    return offer;
   }
-  return null;
+  return offer;
 };
