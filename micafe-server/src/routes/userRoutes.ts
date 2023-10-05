bc1a37c92658a5f533f21afd81316d7979c22052
pedrot111 <pedrochincolla11@gmail.com>
2023-10-05 11:29:20 -0300

    split auth and user routes

--- micafe-server/src/routes/userRoutes.ts
+++ micafe-server/src/routes/userRoutes.ts
@@ -1,33 +1,14 @@
 import express from 'express';
 import {
-  checkEmailExists,
   deleteUser,
-  forgotPassword,
-  getMe,
-  resetPassword,
-  updateUser,
-  validateAccount
+  getUsersByRole,
+  updateUser
 } from '../controllers/userController';
-import {
-  loginValidation,
-  registerValidation
-} from '../middlewares/userValidation';
-import authenticateToken from '../middlewares/tokenValidation';
 
 const router = express.Router();
 
-router.get('/me', authenticateToken, getMe);
-router.post('/signup', registerValidation);
-router.post('/login', loginValidation);
-router.get('/validate/:validationToken', validateAccount);
+router.get('/:role', getUsersByRole);
+router.put('/:id', updateUser);
+router.delete('/:_id', deleteUser);
 
-router.post('/forgot-password', forgotPassword);
-router.post('/forgot-password/:validationToken', resetPassword);
-
-router
-  .route('/:_id')
-  .put(authenticateToken, updateUser)
-  .delete(authenticateToken, deleteUser);
-
-router.post('/check-email', checkEmailExists);
 export default router;
