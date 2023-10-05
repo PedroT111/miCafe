72d7990d2651f49702eb85ee6e6f571afdebe622
pedrot111 <pedrochincolla11@gmail.com>
2023-10-05 11:19:56 -0300

    modify combo model

--- micafe-server/src/models/comboModel.ts
+++ micafe-server/src/models/comboModel.ts
@@ -1,13 +1,22 @@
-import mongoose, { Schema } from 'mongoose';
+import mongoose, { type ObjectId, Schema } from 'mongoose';
 import AppError from '../utils/appError';
+import { Product } from './productModel';
 
 interface ICombo extends Document {
+  _id: ObjectId;
   name: string;
   price: number;
+  points: number;
   description: string;
   urlImage: string;
-  products: [];
+  products: IProductQuantity[];
+  isActive: boolean;
   isDeleted: boolean;
+}
+
+interface IProductQuantity {
+  product: ObjectId;
+  quantity: number;
 }
 
 const comboSchema = new Schema<ICombo>({
@@ -36,6 +45,13 @@ const comboSchema = new Schema<ICombo>({
       quantity: Number
     }
   ],
+  points: {
+    type: Number
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  },
   isDeleted: {
     type: Boolean,
     default: false
@@ -50,6 +66,16 @@ comboSchema.pre('save', async function (next) {
     next(new AppError('The combo must have at least one product', 400));
     return;
   }
+  const products = this.products;
+
+  const productPromises = products.map(async (productItem) => {
+    const product = await Product.findById(productItem.product);
+    return product != null ? product.points * productItem.quantity : 0;
+  });
+
+  const pointsArray = await Promise.all(productPromises);
+  const totalPoints = pointsArray.reduce((acc, points) => acc + points, 0);
+  this.points = totalPoints;
   next();
 });
 
