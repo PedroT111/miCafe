e5313a0ca5685fb993f1a28af8709e4b238c24a3
pedrot111 <pedrochincolla11@gmail.com>
2023-11-06 11:12:51 -0300

    add user constants

---
+++ micafe-client/src/app/features/user-profile/constants/profile.ts
@@ -0,0 +1,30 @@
+export const PROFILE_LINKS = [
+    {
+        name: 'Información de Perfil',
+        href: '/profile/information',
+        icon: 'bi bi-person'
+    },
+    {
+        name: 'Historial de Pedidos',
+        href: '/profile/order-history',
+        icon: 'bi bi-journal-text'
+    }
+]
+
+export const HELP_LINKS = [
+    {
+        name: 'Terminos y Condiciones',
+        href: '',
+        icon: 'bi bi-info-circle'
+    },
+    {
+        name: 'Politicas de Privacidad',
+        href: '',
+        icon: 'bi bi-shield-lock'
+    },
+    {
+        name: 'Contactanos',
+        href: '',
+        icon: 'bi bi-telephone-forward'
+    }
+]
