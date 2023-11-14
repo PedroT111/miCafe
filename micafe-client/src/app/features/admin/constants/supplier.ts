f3f36beb3f25aa6cb384e87df5b2baa98c8b2600
pedrot111 <pedrochincolla11@gmail.com>
2023-11-14 11:53:04 -0300

    add table headers supplier

---
+++ micafe-client/src/app/features/admin/constants/supplier.ts
@@ -0,0 +1,20 @@
+export const SUPPLIER = {
+  SUPPLIER_TABLE_HEADERS: [
+    {
+      header: 'Nombre',
+      name: 'name'
+    },
+    {
+      header: 'Email',
+      name: 'email'
+    },
+    {
+      header: 'Teléfono',
+      name: 'celphone'
+    },
+    {
+      header: 'Dirección',
+      name: 'address'
+    }
+  ]
+};
