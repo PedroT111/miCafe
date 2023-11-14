5edfd56be1997ae3beb94f7c15c6563186494d40
pedrot111 <pedrochincolla11@gmail.com>
2023-11-14 13:01:15 -0300

    subscribe socket

--- micafe-client/src/app/features/employee/order/orders/orders.component.ts
+++ micafe-client/src/app/features/employee/order/orders/orders.component.ts
@@ -3,6 +3,8 @@ import { OrdersService } from '../../../../shared/services/orders.service';
 import { Subscription } from 'rxjs';
 import { Order } from 'src/app/shared/models/order';
 import { SwalService } from 'src/app/shared/utils/swal.service';
+import { WebsocketService } from 'src/app/core/services/websocket.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-orders',
@@ -19,24 +21,43 @@ export class OrdersComponent implements OnInit, OnDestroy {
 
   constructor(
     private orderService: OrdersService,
-    private swal: SwalService
+    private swal: SwalService,
+    private socketService: WebsocketService,
+    private toastr: ToastrService
   ) {}
 
   ngOnDestroy(): void {
     this.sub.unsubscribe();
+    this.socketService.disconnect();
   }
 
   ngOnInit(): void {
+    this.socketService.connect();
+    this.listenToNewOrderEvent();  
     this.getOrders();
+
+  }
+
+  private listenToNewOrderEvent(): void {
+    this.socketService.listen('newOrder').subscribe({
+      next: (data: {msg: string, order: Order}) => {
+        this.toastr.info(`${data.msg}: ${data.order.orderNumber}`);
+        this.getOrders();
+      },
+      error: (error) => {
+        console.error('Error al escuchar el evento newOrder:', error);
+      }
+    });
   }
 
   getOrders(){
+    this.getPickedUpOrders();
     this.getOrdersByStatus('pending');
     this.getOrdersByStatus('inProcess');
-    this.getOrdersByStatus('pickedUp');
 
     this.inProcessOrderCount();
   }
+
   inProcessOrderCount(){
     this.inProcessCount = this.inProcessOrders.length;
   }
@@ -45,22 +66,32 @@ export class OrdersComponent implements OnInit, OnDestroy {
     this.sub.add(
       this.orderService.getOrdersByStatus(status).subscribe({
         next: (res) => {
+          console.log(res);
           if(status === 'pending'){
-            this.pendingOrders = res.orders;
+            this.pendingOrders = res;
           } else if(status === 'inProcess'){
-            this.inProcessOrders = res.orders;
+            this.inProcessOrders = res;
             this.inProcessOrderCount();
-          } else if(status === 'pickedUp'){
-            this.pickedUpOrders = res.orders;
-          }
-          
-         
+          } 
         },
         error: (err) => {
           console.log(err);
         }
       })
     );
+  }
+
+  getPickedUpOrders(){
+    this.sub.add(
+      this.orderService.getOrdersByStatusAndEmployee('pickedUp').subscribe({
+        next: (res) => {
+            this.pickedUpOrders = res;
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      })
+    )
   }
   
 
