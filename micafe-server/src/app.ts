8a08df7a4d00f89339a0ab0ebaec53f146758a4f
pedrot111 <pedrochincolla11@gmail.com>
2023-11-14 11:48:06 -0300

    add supplier routes

--- micafe-server/src/app.ts
+++ micafe-server/src/app.ts
@@ -1,26 +1,45 @@
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import express, { type ErrorRequestHandler } from 'express';
 import cors from 'cors';
+import http from 'http';
+import authRoutes from './routes/authRoutes';
 import userRoutes from './routes/userRoutes';
 import categoryProductRoutes from './routes/categoryProductRoutes';
 import productRoutes from './routes/productRoutes';
 import comboRoutes from './routes/comboRoutes';
 import offerRoutes from './routes/offerRoutes';
+import orderRoutes from './routes/orderRoutes';
+import discountRoutes from './routes/discountRoutes';
+import employeeRoutes from './routes/employeeRoutes';
+import reportRoutes from './routes/reportRoutes';
+import supplierRoutes from './routes/supplierRoutes';
 import AppError from './utils/appError';
 import offerScheduler from './tasks/offerScheduler';
+import discountScheduler from './tasks/discountScheduler';
+import {initializeSocket} from './sockets';
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:4200' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/authenticate', userRoutes);
+const server = http.createServer(app);
+initializeSocket(server);
+
+
+app.use('/authenticate', authRoutes);
+app.use('/user', userRoutes);
 app.use('/category-product', categoryProductRoutes);
 app.use('/product', productRoutes);
 app.use('/combo', comboRoutes);
 app.use('/offer', offerRoutes);
-
+app.use('/orders', orderRoutes);
+app.use('/discount', discountRoutes);
+app.use('/employee', employeeRoutes);
+app.use('/report', reportRoutes);
+app.use('/supplier', supplierRoutes)
 offerScheduler();
+discountScheduler();
 
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   let statusCode = 500;
@@ -38,4 +57,4 @@ const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
 
 app.use(errorHandler);
 
-export default app;
+export { app, server };
