82c94a1e4e4e73f50cb982874eac6ae0cba3e146
pedrot111 <pedrochincolla11@gmail.com>
2023-11-14 12:14:47 -0300

    create orde calification component

---
+++ micafe-client/src/app/features/user-profile/components/order-qualification/order-qualification.component.ts
@@ -0,0 +1,41 @@
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+import { Subscription } from 'rxjs';
+import { OrdersService } from 'src/app/shared/services/orders.service';
+
+@Component({
+  selector: 'app-order-qualification',
+  templateUrl: './order-qualification.component.html',
+  styleUrls: ['./order-qualification.component.css']
+})
+export class OrderQualificationComponent implements OnInit, OnDestroy {
+  sub: Subscription = new Subscription();
+  @Input() orderId: string;
+  currentRate = 0;
+  constructor(
+    private NgbActiveModal: NgbActiveModal,
+    private orderService: OrdersService,
+    private toastr: ToastrService
+  ) {}
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  ngOnInit(): void {}
+
+  qualify() {
+    console.log(this.orderId);
+    this.sub.add(
+      this.orderService.orderRate(this.orderId, this.currentRate).subscribe({
+        next: (res) => {
+          this.toastr.success(res.msg);
+        },
+        error: (err) => {
+          this.toastr.error(err.error.error);
+        }
+      })
+    );
+    this.NgbActiveModal.close('qualification');
+  }
+}
