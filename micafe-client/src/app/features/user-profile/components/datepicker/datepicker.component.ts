7ee333eddb87c098d7d818a82e1022c480114e29
pedrot111 <pedrochincolla11@gmail.com>
2023-11-14 14:24:42 -0300

    create datepicker component

---
+++ micafe-client/src/app/features/user-profile/components/datepicker/datepicker.component.ts
@@ -0,0 +1,74 @@
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import {
+  NgbActiveModal,
+  NgbCalendar,
+  NgbDate,
+  NgbDateStruct,
+  NgbTimeStruct
+} from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+import { Subscription } from 'rxjs';
+import { OrdersService } from 'src/app/shared/services/orders.service';
+
+@Component({
+  selector: 'app-datepicker',
+  templateUrl: './datepicker.component.html',
+  styleUrls: ['./datepicker.component.css']
+})
+export class DatepickerComponent implements OnInit, OnDestroy {
+  sub: Subscription = new Subscription();
+  @Output() onSelectDateTime = new EventEmitter();
+  @Input() orderDate: Date;
+  @Input() orderId: string;
+  date: NgbDateStruct;
+  minDate: NgbDate;
+  time: NgbTimeStruct;
+  pickUpDateTime: Date;
+  constructor(
+    private modalService: NgbActiveModal,
+    private calendar: NgbCalendar,
+    private orderService: OrdersService,
+    private toastr: ToastrService
+  ) {}
+
+  ngOnDestroy(): void {
+   this.sub.unsubscribe();
+  }
+
+  ngOnInit(): void {
+    this.minDate = this.calendar.getToday();
+    console.log(this.orderDate, 'od');
+    if (this.orderDate) {
+      const date = new Date(this.orderDate);
+      this.date = {
+        year: date.getFullYear(),
+        month: date.getMonth() + 1,
+        day: date.getDate()
+      };
+      this.time = {
+        hour: date.getHours(),
+        minute: date.getMinutes(),
+        second: date.getSeconds()
+      };
+    }
+  }
+
+  setDateTime() {
+    const { year, month, day } = this.date;
+    const { hour, minute } = this.time;
+
+    this.pickUpDateTime = new Date(year, month - 1, day, hour, minute);
+
+    this.sub.add(
+      this.orderService.changePickUpTime(this.orderId, this.pickUpDateTime).subscribe({
+        next: (res) => {
+          this.toastr.success(res.msg);
+          this.modalService.close(res.msg);
+        },
+        error: (err) => {
+          this.toastr.error(err.error.error);
+        }
+      })
+    )
+  }
+}
