0e76969882bed9f4fc5e584097690dc604fc0dea
pedrot111 <pedrochincolla11@gmail.com>
2023-11-14 12:11:54 -0300

    add order constants

---
+++ micafe-client/src/app/features/admin/constants/order.ts
@@ -0,0 +1,29 @@
+export const ORDERS = {
+    ORDERS_TABLE_HEADERS: [
+        {
+            header: 'Orden',
+            name: 'orderNumber'
+        },
+        {
+            header: 'Fecha',
+            name: 'date'
+        },
+        {
+            header: 'Cliente',
+            name: 'customer'
+        },
+
+        {
+            header: 'Total',
+            name: 'totalAmount'
+        },
+        {
+            header: 'Empleado',
+            name: 'employee'
+        },
+        {
+            header: 'Estado',
+            name: 'status',
+        }
+    ]
+}
