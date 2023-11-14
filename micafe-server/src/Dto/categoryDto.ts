3fc8e53f7479f2c88d649baea379ce55081c2fb6
pedrot111 <pedrochincolla11@gmail.com>
2023-11-14 14:27:01 -0300

    create category product dto

--- micafe-server/src/Dto/categoryDto.ts
+++ micafe-server/src/Dto/categoryDto.ts
@@ -1,7 +1,14 @@
 import { type ObjectId } from 'mongoose';
+import { IProduct } from '../models/productModel';
 
 export interface CategoryDto {
   _id: ObjectId;
   name: string;
   totalProducts: number;
+}
+
+export interface CategoryProductsDTO{
+  _id: ObjectId;
+  name: string;
+  products: IProduct[];
 }
