27724421322ab912531d57b5f5ff7af099984be1
pedrot111 <pedrochincolla11@gmail.com>
2023-11-14 14:30:29 -0300

    create date format pipe

---
+++ micafe-client/src/app/shared/pipes/date-format.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'dateFormat'
+})
+export class DateFormatPipe implements PipeTransform {
+
+  transform(value: Date): string {
+    if (!value) return '';
+
+    const options: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: 'short',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false
+    };
+
+    return value.toLocaleString('es-ES', options);
+  }
+
+}
