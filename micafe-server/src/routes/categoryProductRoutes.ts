9a134d98ef07c5e8deeb573ea435857364a8e4f2
pedrot111 <pedrochincolla11@gmail.com>
2023-11-14 13:11:33 -0300

    add category product route

--- micafe-server/src/routes/categoryProductRoutes.ts
+++ micafe-server/src/routes/categoryProductRoutes.ts
@@ -3,16 +3,19 @@ import {
   createCategory,
   deleteCategory,
   getCategories,
+  getCategoriesProducts,
   getOne,
   updateCategory
 } from '../controllers/categoryProductController';
 import { isAdmin } from '../middlewares/roleUser';
 import { idValidationRule } from '../validators/commonValidator';
 
+
 const router = express.Router();
 
 router.post('/new', createCategory);
 router.get('/categories', getCategories);
+router.get('/categories/products', getCategoriesProducts);
 router.route('/:id').get(getOne).put(updateCategory);
 router.route('/delete/:id').delete(idValidationRule, deleteCategory);
 
