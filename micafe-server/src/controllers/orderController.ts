075f2e477ee0a91b5462cfaee39db2a693914a54
pedrot111 <pedrochincolla11@gmail.com>
2023-11-14 13:03:32 -0300

    create order methods adn routes

--- micafe-server/src/controllers/orderController.ts
+++ micafe-server/src/controllers/orderController.ts
@@ -1,9 +1,106 @@
 import { type NextFunction, type Request, type Response } from 'express';
 import catchAsync from '../utils/catchAsync';
 import AppError from '../utils/appError';
-import { changeOrderStatus, create, getDetailOrders } from '../services/order';
+import {
+  assignOrder,
+  changeOrderStatus,
+  create,
+  getAll,
+  getDetailOrders,
+  getOne,
+  getOrdersByStatusAndEmployee,
+  getOrdersByUser,
+  paymentOrder,
+  rateOrder,
+  updateOrderEmployee,
+  updatePickUpDateTime
+} from '../services/order';
 import { validationResult } from 'express-validator';
-import { io } from '../app';
+import { io } from '../sockets';
+import { IOrder } from '../models/orderModel';
+import { calculateTotalOrder } from '../utils/orderCalculations';
+import { MercadoPagoConfig, Preference } from 'mercadopago';
+import { getOneById } from '../services/user';
+
+const client = new MercadoPagoConfig({
+  accessToken: process.env.MP_ACCES_TOKEN || ''
+});
+
+export const getAllOrders = catchAsync(
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const orders = await getAll();
+
+    if (orders.length === 0) {
+      next(new AppError('There are no orders', 400));
+      return;
+    }
+
+    res.status(200).json({
+      ok: true,
+      orders
+    });
+  }
+);
+
+export const getOrder = catchAsync(
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+      return;
+    }
+    const { id } = req.params;
+    const order = await getOne(id);
+
+    if (order === null) {
+      next(new AppError('There is no order with that id', 400));
+      return;
+    }
+
+    res.status(200).json({
+      ok: true,
+      order
+    });
+  }
+);
+export const getAllOrdersByUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+      return;
+    }
+    const { id } = req.params;
+    /* if (id !== req.headers.user) {
+      next(new AppError('Something was wrong!', 400));
+      return;
+    }*/
+
+    const orders = await getOrdersByUser(id);
+
+    if (orders.length === 0) {
+      next(new AppError('There are no orders', 400));
+      return;
+    }
+
+    res.status(200).json({
+      ok: true,
+      orders
+    });
+  }
+);
+
+export const getAllOrdersByStatusAndEmployee = catchAsync(
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const { status, employee } = req.params;
+    const orders = await getOrdersByStatusAndEmployee(status, employee);
+
+    res.status(200).json({
+      ok: true,
+      orders
+    });
+  }
+);
 
 export const createNewOrder = catchAsync(
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
@@ -12,44 +109,152 @@ export const createNewOrder = catchAsync(
       res.status(400).json({ errors: errors.array() });
       return;
     }
-    const order = req.body;
+    const order: IOrder = req.body;
     if (order.products.length === 0 && order.combos.length === 0) {
-      next(new AppError('No se puede crear una orden sin productos ', 400));
+      next(new AppError('Cannot create an order without products', 400));
       return;
     }
     if (order.pickUpDateTime === null) {
-      next(new AppError('La orden no tiene un horario de retiro', 400));
+      next(new AppError('The order does not have a pickup time', 400));
     }
 
-    const newOrder = await create(order);
-    if (newOrder === null) {
+    if(order.products.length === 0 && order.combos.length === 0){
+      next(new AppError('The order is empty', 400));
+    }
+    console.log(order, 'ord')
+    const { totalAmount, totalPoints } = await calculateTotalOrder(order);
+    order.totalAmount = totalAmount;
+    order.totalPoints = totalPoints;
+
+    const query = await create(order);
+    if (query === null) {
       next(new AppError('Something was wrong!', 400));
     }
-    io.emit('new order', {order: newOrder});
-    res.status(200).json({
-      ok: true,
-      order: newOrder
-    });
+
+    const preference = new Preference(client);
+    preference
+      .create({
+        body: {
+          notification_url: '',
+          items: [
+            {
+              id: '',
+              title: 'MiCafeApp',
+              quantity: 1,
+              unit_price: 1000
+            }
+          ],
+          back_urls: {
+            success: `http://localhost:4200/shopping/payment-success/${order._id}`,
+            failure: 'http://localhost:4200/shopping/payment-failed'
+          },
+          auto_return: 'approved'
+        }
+      })
+      .then((result) =>
+        res.status(200).json({
+          ok: true,
+          reference: result.id,
+          order: query
+        })
+      )
+      .catch((error) => console.log(error));
+
+    io.emit('newOrder', { msg: 'Nueva orden', order: query });
   }
 );
 
-export const getOrdersByStatus = catchAsync(
+export const paidOrderWithPoints = catchAsync(
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       res.status(400).json({ errors: errors.array() });
       return;
     }
+    const order: IOrder = req.body;
+    if (order.products.length === 0 && order.combos.length === 0) {
+      next(new AppError('Cannot create an order without products', 400));
+      return;
+    }
+    if (order.pickUpDateTime === null) {
+      next(new AppError('The order does not have a pickup time', 400));
+      return;
+    }
+
+    const { totalAmount, totalPoints } = await calculateTotalOrder(order);
+    order.totalAmount = totalAmount;
+    order.totalPoints = totalPoints;
+
+    const { customer } = req.body;
+    const user = await getOneById(customer);
+
+    if (user === null) {
+      next(new AppError('Something was wrong!', 400));
+      return;
+    }
+    console.log(user.points, 'userp', order.totalPoints ,'orp')
+    if (user.points && user.points < order.totalPoints) {
+      console.log('error')
+      next(new AppError('insufficient points', 400));
+      return;
+    }
+    order.paidWithPoints = true;
+    order.paymentStatus = 'success';
+    const query = await create(order);
+    if (query === null) {
+      next(new AppError('Something was wrong!', 400));
+    }
+
+    io.emit('newOrder', { msg: 'Nueva orden', order: query });
+
+    res.status(200).json({
+      ok: true,
+      order: query
+    });
+  }
+);
+
+export const markOrderAsPaid = catchAsync(
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    if (req.params.type === 'payment') {
+      console.log(req.params);
+    }
+  }
+);
+
+export const getOrdersByStatus = catchAsync(
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const { status } = req.params;
     const orders = await getDetailOrders(status);
-    if (orders.length === 0) {
-      next(new AppError(`There are no ${status} orders`, 400));
+
+    res.status(200).json({
+      ok: true,
+      orders
+    });
+  }
+);
+
+export const orderRate = catchAsync(
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+      return;
+    }
+    const { orderId } = req.params;
+    const { qualification } = req.body;
+
+
+    const order = await rateOrder(orderId, qualification);
+
+    if (order === null) {
+      next(new AppError('Something was wrong!', 404));
       return;
     }
 
     res.status(200).json({
       ok: true,
-      orders
+      msg: 'Order qualification updated.',
     });
   }
 );
@@ -63,8 +268,33 @@ export const changeOrderState = catchAsync(
     }
     const { id } = req.params;
     const { status } = req.body;
+    const order: IOrder | null = await changeOrderStatus(id, status);
 
-    const order = await changeOrderStatus(id, status);
+    if (order === null) {
+      next(new AppError('Something was wrong!', 404));
+      return;
+    }
+
+    res.status(200).json({
+      ok: true,
+      msg: 'Order status was updated successfully.',
+    });
+    io.emit('updatedOrder', { msg: 'Actualización de estado'});
+    
+  }
+);
+
+export const assignOrderToEmployee = catchAsync(
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+      return;
+    }
+    const { id } = req.params;
+    const { employee } = req.body;
+
+    const order = await assignOrder(id, employee);
 
     if (!order) {
       next(new AppError('Something was wrong!', 404));
@@ -73,7 +303,55 @@ export const changeOrderState = catchAsync(
 
     res.status(200).json({
       ok: true,
-      msg: 'Order status was updated successfully.'
+      msg: 'Order has beeen assigned.'
+    });
+  }
+);
+
+export const updateEmployeeOrder = catchAsync(
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+      return;
+    }
+    const { id } = req.params;
+    const { employee } = req.body;
+
+    const order = await updateOrderEmployee(id, employee);
+
+    if (!order) {
+      next(new AppError('Something was wrong!', 404));
+      return;
+    }
+
+    res.status(200).json({
+      ok: true,
+      order
+    });
+  }
+);
+
+export const changeOrderPickUpDate = catchAsync(
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+      return;
+    }
+    const { orderId } = req.params;
+    const { pickUpDateTime } = req.body;
+
+    const order = await updatePickUpDateTime(orderId, pickUpDateTime);
+
+    if (!order) {
+      next(new AppError('Something was wrong!', 404));
+      return;
+    }
+
+    res.status(200).json({
+      ok: true,
+      msg: 'Order pick up date time updated!'
     });
   }
 );
--- micafe-server/src/services/order.ts
+++ micafe-server/src/services/order.ts
@@ -1,9 +1,51 @@
+import { ObjectId } from 'mongoose';
 import { Order, type IOrder } from '../models/orderModel';
+import { applyDiscount } from '../utils/applyDiscount';
+import { isAfter, isSameDay} from 'date-fns';
 
 export const create = async (order: IOrder): Promise<IOrder | null> => {
-  const newOrder = new Order(order);
-  await newOrder.save();
-  return newOrder;
+  let newOrder = order;
+  let query = null;
+  try {
+    if (order.discountCode != null) {
+      const orderWithDiscount = await applyDiscount(
+        order,
+        order.discountCode,
+        order.customer.toString()
+      );
+      if (orderWithDiscount === null) {
+        return null;
+      }
+      if (orderWithDiscount) {
+        newOrder = orderWithDiscount;
+      }
+    }
+
+    const orderInstance = new Order(newOrder);
+    await orderInstance.save();
+    query = orderInstance;
+  } catch (err) {
+    console.log(err);
+  }
+  return query;
+};
+
+export const getAll = async (): Promise<IOrder[]> => {
+  return await Order.find()
+    .populate('customer')
+    .populate('products.product')
+    .populate('combos.combo')
+    .populate('employee')
+    .sort({ date: 1 });
+};
+
+export const getOne = async (id: string): Promise<IOrder | null> => {
+  return await Order.findById(id)
+    .populate('customer')
+    .populate('products.product')
+    .populate('combos.combo')
+    .populate('employee')
+    .populate('discountCode');
 };
 
 export const getDetailOrders = async (status: string): Promise<IOrder[]> => {
@@ -12,27 +54,158 @@ export const getDetailOrders = async (status: string): Promise<IOrder[]> => {
     .populate('products.product')
     .populate('combos.combo')
     .populate('employee')
-    .sort({pickUpDateTime: 1})
+    .sort({ pickUpDateTime: 1 });
 };
-export const getOrdersByStatusAndEmployee = async (status: string, id:string): Promise<IOrder[]> => {
+export const getOrdersByStatusAndEmployee = async (
+  status: string,
+  id: string
+): Promise<IOrder[]> => {
   return await Order.find({ status, employee: id })
     .populate('customer')
     .populate('products.product')
     .populate('combos.combo')
     .populate('employee')
-    .sort({pickUpDateTime: 1})
+    .sort({ pickUpDateTime: -1 });
 };
 
 export const changeOrderStatus = async (
   id: string,
-  status: 'picked up' | 'in process' | 'cancelled'
+  status: 'pickedUp' | 'cancelled' | 'inProcess' | 'pending'
+): Promise<IOrder | null> => {
+  console.log(status, 'st');
+  try {
+    const order = await Order.findById(id);
+    if (!order) {
+      return null;
+    }
+    if (status === 'pickedUp') {
+      order.actualPickUpDateTime = new Date();
+    }
+    if (status === 'pending') {
+      order.employee = null;
+      order.actualPickUpDateTime = null;
+    }
+
+    order.status = status;
+    await order.save();
+    return order;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
+
+export const assignOrder = async (
+  id: string,
+  employeeId: ObjectId
 ): Promise<boolean> => {
   const order = await Order.findById(id);
   if (!order) {
     return false;
   }
+  order.status = 'inProcess';
+  order.employee = employeeId;
 
-  order.status = status;
   await order.save();
   return true;
+};
+
+export const paymentOrder = async (id: string): Promise<IOrder | null> => {
+  const order = await Order.findById(id);
+  try {
+    if (!order) {
+      return null;
+    }
+
+    order.paymentStatus = 'success';
+    await order.save();
+  } catch (err) {
+    console.log(err);
+  }
+  return order;
+};
+
+export const updateOrderEmployee = async (
+  id: string,
+  employeeId: ObjectId
+): Promise<IOrder | null> => {
+  const order = await Order.findById(id);
+  try {
+    if (!order) {
+      return null;
+    }
+
+    order.employee = employeeId;
+    await order.save();
+  } catch (err) {
+    console.log(err);
+  }
+  return order;
+};
+
+export const getOrdersByUser = async (id: string): Promise<IOrder[]> => {
+  console.log(id);
+  return await Order.find({ customer: id })
+    .populate('customer')
+    .populate('products.product')
+    .populate('combos.combo')
+    .sort({ pickUpDateTime: -1 });
+};
+
+export const rateOrder = async (
+  orderId: string,
+  rate: number
+): Promise<IOrder | null> => {
+  return await Order.findByIdAndUpdate(
+    orderId,
+    {
+      qualification: rate
+    },
+    { new: true }
+  );
+};
+
+export const updatePickUpDateTime = async (
+  orderId: string,
+  pickUpDateTime: Date
+): Promise<IOrder | null> => {
+  const order = await Order.findById(orderId);
+
+  if (!order) {
+    return null;
+  } 
+
+  if(order.status != 'pending'){
+    return null;
+  }
+  const currentTime = new Date();
+  if(isAfter(new Date(pickUpDateTime), currentTime)){
+    order.pickUpDateTime = pickUpDateTime;
+    await order.save();
+    return order;
+  }
+  /*
+  const currentTime = new Date();
+  const sameDay = isSameDay(order.pickUpDateTime, currentTime);
+  console.log(order.pickUpDateTime, 'order date')
+  console.log(pickUpDateTime, 'data')
+  console.log(currentTime, 'ct')
+  if (order.pickUpDateTime instanceof Date && currentTime instanceof Date && sameDay) {
+    const timeDifference = (order.pickUpDateTime.getTime() - currentTime.getTime()) / 60000; // Corrección en la fórmula (60000 en lugar de 6000)
+    console.log(timeDifference, 'td')
+    if (timeDifference <= 10) { 
+      return null;
+    }
+  }
+  console.log(pickUpDateTime, 'pckdt')
+  console.log(currentTime, 'ct')
+  if (isAfter(new Date(pickUpDateTime), currentTime)) {
+    console.log('hola')
+    console.log(new Date(pickUpDateTime), 'holasda')
+    order.pickUpDateTime = pickUpDateTime;
+    await order.save();
+    return order; 
+  }*/
+
+  return null;
 };
