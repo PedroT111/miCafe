2c8979eb18301e2cd54993e11cf2ad80f3e77575
pedrot111 <pedrochincolla11@gmail.com>
2024-01-22 19:15:58 -0300

    implements role guard

--- micafe-client/src/app/app-routing.module.ts
+++ micafe-client/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './features/home/index/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { PAGES } from './shared/constants/index';
+import { AuthGuard } from './core/guards/auth.guard';
 const routes: Routes = [
   {
     path: PAGES.INDEX,
@@ -15,23 +16,35 @@ const routes: Routes = [
   },
   {
     path: PAGES.ADMIN,
-    loadChildren: () => 
-      import('./features/admin/admin.module').then((m) => m.AdminModule)
+    loadChildren: () =>
+      import('./features/admin/admin.module').then((m) => m.AdminModule),
+    canActivate: [AuthGuard],
+    data: { expectedRole: 'admin' }
   },
   {
     path: 'employee',
-    loadChildren: () => 
-      import('./features/employee/employee.module').then((m) => m.EmployeeModule)
+    loadChildren: () =>
+      import('./features/employee/employee.module').then(
+        (m) => m.EmployeeModule
+      ),
+    canActivate: [AuthGuard],
+    data: { expectedRole: 'employee' }
   },
   {
     path: 'shopping',
-    loadChildren: () => 
-      import('./features/shopping/shopping.module').then((m) => m.ShoppingModule)
+    loadChildren: () =>
+      import('./features/shopping/shopping.module').then(
+        (m) => m.ShoppingModule
+      ),
+    canActivate: [AuthGuard],
+    data: { expectedRole: 'user' }
   },
   {
     path: 'profile',
-    loadChildren: () => 
-      import('./features/user-profile/user-profile.module').then((m) => m.UserProfileModule)
+    loadChildren: () =>
+      import('./features/user-profile/user-profile.module').then(
+        (m) => m.UserProfileModule
+      )
   },
   {
     path: '**',
@@ -42,8 +55,7 @@ const routes: Routes = [
 const routerOptions: ExtraOptions = {
   scrollPositionRestoration: 'enabled',
   anchorScrolling: 'enabled'
-  
-}
+};
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, routerOptions)],
