b640c7a363556e5736a2b3b21492e43c625b519d
pedrot111 <pedrochincolla11@gmail.com>
2024-02-14 18:27:01 -0300

    Update background images, adjust text colors, and enhance button width

--- micafe-client/src/app/features/home/components/home-banner/home-banner.component.ts
+++ micafe-client/src/app/features/home/components/home-banner/home-banner.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as Aos from 'aos';
+import { HOME } from '../../constants/index';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-banner',
@@ -7,14 +9,20 @@ import * as Aos from 'aos';
   styleUrls: ['./home-banner.component.css']
 })
 export class HomeBannerComponent implements OnInit {
-
-  constructor() { }
+  title = HOME.TITLE_BANNER;
+  text = HOME.TEXT_BANNER;
+  button = HOME.BUTTON_BANNER;
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
     Aos.init({
       duration: 1000,
       easing: 'ease-in-out'
     });
+  }
+
+  goSignUp(){
+    this.router.navigate(['/auth/signup']);
   }
 
 }
